52f7d5ce51de47ba83221270384fd2f60eeb2404
umer <umerfarid53@gmail.com>
2020-04-05 01:44:25 -0400

    Installer

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ if [ "$mama" == '1' ]; then
         BASH_PATH="/bin/bash"
         TERMUX=false
 
-        sudo apt update; sudo apt-get install -y git python3; sudo apt install python3-pip
+        sudo apt update; sudo apt-get install -y git python3; sudo apt install python3-pip; sudo pip3 install netifaces
     fi
 
     echo -e "\n[\e[32m✔\e[97m] Checking directories...";
